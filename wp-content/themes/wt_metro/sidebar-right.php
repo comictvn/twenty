b3c88f64cb06ed0801b7fd6e6d7ef6a0ac0df374
Hung Nguyen <tidus2102@gmail.com>
2014-01-20 22:48:59 +0700

    fix css

--- wp-content/themes/wt_metro/sidebar-right.php
+++ wp-content/themes/wt_metro/sidebar-right.php
@@ -8,37 +8,37 @@
  * @link 	 http://wellthemes.com
  */
  ?> 
-<div id="right-sidebar" class="sidebar">
+<!--<div id="right-sidebar" class="sidebar">
 		
-		<?php if ( ! dynamic_sidebar( 'sidebar-2' ) ) : ?>			
+		<?php# if ( ! dynamic_sidebar( 'sidebar-2' ) ) : ?>			
 				
 			<div class="widget widget_text">
-				<h3><?php _e( 'Hello', 'wellthemes' ); ?></h3>			
-				<div class="textwidget"><?php _e( 'This sidebar is here because you have not selected any widgets to display in this sidebar. Go to your dashboard and select widgets for this sidebar.', 'wellthemes' ); ?></div>
+				<h3><?php# _e( 'Hello', 'wellthemes' ); ?></h3>			
+				<div class="textwidget"><?php# _e( 'This sidebar is here because you have not selected any widgets to display in this sidebar. Go to your dashboard and select widgets for this sidebar.', 'wellthemes' ); ?></div>
 			</div>
 			
 			<div class="widget widget_search">
-				<?php get_search_form(); ?>
+				<?php# get_search_form(); ?>
 			</div>
 			
-			<?php the_widget('WP_Widget_Recent_Posts', 'number=5', 'before_title=<h3>&after_title=</h3>'); ?>					
+			<?php# the_widget('WP_Widget_Recent_Posts', 'number=5', 'before_title=<h3>&after_title=</h3>'); ?>					
 					
 			<div class="widget widget_categories">
-				<h3><?php _e( 'Popular Categories', 'wellthemes' ); ?></h3>
-				<ul><?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'title_li' => '', 'number' => 10 ) ); ?></ul>
+				<h3><?php# _e( 'Popular Categories', 'wellthemes' ); ?></h3>
+				<ul><?php# wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'title_li' => '', 'number' => 10 ) ); ?></ul>
 			</div>
 					
 			<div class="widget widget_archive">
-				<h3 class="widget-title"><?php _e( 'Archives', 'wellthemes' ); ?></h3>
+				<h3 class="widget-title"><?php# _e( 'Archives', 'wellthemes' ); ?></h3>
 				<ul>
-					<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+					<?php# wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 				</ul>
 			</div>
 			
-			<?php the_widget( 'WP_Widget_Tag_Cloud', array('title' => 'Popular tags', ), array('before_title' => '<h3>', 'after_title' => '</h3>')); ?>
+			<?php# the_widget( 'WP_Widget_Tag_Cloud', array('title' => 'Popular tags', ), array('before_title' => '<h3>', 'after_title' => '</h3>')); ?>
 			
 				
-		<?php endif; // end sidebar widget area ?>
+		<?php# endif; // end sidebar widget area ?>
 		
 </div><!-- /sidebar -->
 		
