1b3d0735ba7bdea7b70b16e0429a9b6c66b8a09b
Hung Nguyen <tidus2102@gmail.com>
2014-03-18 22:19:41 +0700

    fix

--- wp-content/plugins/facebook-comments-plugin/class-frontend.php
+++ wp-content/plugins/facebook-comments-plugin/class-frontend.php
@@ -103,7 +103,7 @@ if (!isset($options['count'])) {$options['count'] = "off";}
 return $content;
 }
 /* add_filter ('the_content', 'fbcommentbox', 100); */
-add_filter('comments_open', 'fbcommentbox', 100);
+add_filter('entry-tags', 'fbcommentbox', 100);
 
 
 function fbcommentshortcode($fbatts) {
