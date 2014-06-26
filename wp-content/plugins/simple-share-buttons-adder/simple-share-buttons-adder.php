2726da5ea77eb44d345ba2b10f8c3a9e7fd37c14
Hung Nguyen <tidus2102@gmail.com>
2014-06-26 13:06:21 +0700

    fix stardibi intergrated

--- wp-content/plugins/simple-share-buttons-adder/simple-share-buttons-adder.php
+++ wp-content/plugins/simple-share-buttons-adder/simple-share-buttons-adder.php
@@ -713,40 +713,60 @@ GNU General Public License for more details.
 			}
 		}
 		
-		$htmlContent .= '<script>
-							jQuery(document).ready(function(){
-								var startuplist = jQuery(".stardibi-startup-info");
-								if (startuplist) {
-									$(".stardibi-startup-info").each(function() {
-										setTimeout(function() {
-											var startupdiv = jQuery(this);
-											var startup = startupdiv.attr("startup");
-											var stext = startupdiv.attr("show-text");
-										
-											var url = "http://www.stardibi.com/integrate/getStartupInfo?client_key=77Qw163yk3yu7BnrWXA863orAuQ3It8J25343264v2e22687h8dG174M65BxeQOz&api_key=sExBxcxuWSwhZynblvmsOPCvZceXzFKKFFUEtt4YNqXKSDfLltCOhsnpAEgf2PUP&startup=" + startup;
+		$htmlContent .= '
+<script>
+	function loadInfo(startupdiv) {
+		var startup = startupdiv.attr("startup");
+		var stext = startupdiv.attr("show-text");
+			
+		var url = "http://www.stardibi.com/integrate/getStartupInfo?client_key=77Qw163yk3yu7BnrWXA863orAuQ3It8J25343264v2e22687h8dG174M65BxeQOz&api_key=sExBxcxuWSwhZynblvmsOPCvZceXzFKKFFUEtt4YNqXKSDfLltCOhsnpAEgf2PUP&startup=" + startup;
 
-											jQuery.get(url, function(data){
-												var html = "";
-													var entry = data;
-													html += "<div class=\"startup-info\">";
-														html += "<a href=\"http://www.stardibi.com" + entry.link + "\"><img class=\"slogo\" src=\"" + entry.logo + "\" alt=\"" + entry.name + "\" title=\"" + entry.name + "\"></a>";
-														html += "<div class=\"sinfo\">";
-															html += "<h3><a href=\"http://www.stardibi.com" + entry.link + "\" title=\"" + entry.name + "\">" + entry.name + "</a></h3>";
-															html += "<div class=\"entry-meta\">";
-																html += entry.headline + "<br/>";
-																html += "Market: " + entry.category + "<br/>";
-																html += "Founded: " + entry.founded;
-															html += "</div>";
-														html += "</div>";
-													html += "</div>";
-												html += "<a class=\"btn-cta\" href=\"http://www.stardibi.com/startups\" target=\"_blank\">" + stext + "</a>";
-												startupdiv.html(html);
-											},"jsonp");
-										}, 2000);
-									});
-								}
-							});
-						</script>';
+		jQuery.get(url, function(data){
+			var html = "";
+				var entry = data;
+				html += "<div class=\"startup-info\">";
+					html += "<a href=\"http://www.stardibi.com" + entry.link + "\"><img class=\"slogo\" src=\"" + entry.logo + "\" alt=\"" + entry.name + "\" title=\"" + entry.name + "\"></a>";
+					html += "<div class=\"sinfo\">";
+						html += "<h3><a href=\"http://www.stardibi.com" + entry.link + "\" title=\"" + entry.name + "\">" + entry.name + "</a></h3>";
+						html += "<div class=\"entry-meta\">";
+							html += entry.headline + "<br/>";
+							html += "Market: " + entry.category + "<br/>";
+							html += "Founded: " + entry.founded;
+						html += "</div>";
+					html += "</div>";
+				html += "</div>";
+			html += "<a class=\"btn-cta\" href=\"http://www.stardibi.com/startups\" target=\"_blank\">" + stext + "</a>";
+			startupdiv.html(html);
+		},"jsonp");
+	}
+
+	jQuery(document).ready(function(){
+		var startup = jQuery("#stardibi-startup-info");
+		if (startup) {
+			loadInfo(startup);
+		}
+		
+		var startup = jQuery("#stardibi-startup-info1");
+		if (startup) {
+			loadInfo(startup);
+		}
+		
+		var startup = jQuery("#stardibi-startup-info2");
+		if (startup) {
+			loadInfo(startup);
+		}
+		
+		var startup = jQuery("#stardibi-startup-info3");
+		if (startup) {
+			loadInfo(startup);
+		}
+		
+		var startup = jQuery("#stardibi-startup-info4");
+		if (startup) {
+			loadInfo(startup);
+		}
+	});
+</script>';
 		
 		$htmlContent .= '<div class="entry-social">			
 							<span class="es fb">
